a2db04c5ff38d43338fea5ec3f648759f2beceb2
congphuong1606 <cphuong.coldboy@gmail.com>
2017-09-27 01:42:19 +0700

    ok 90 %

--- app/src/main/java/vn/congphuong/ominext/soanvanthpt/ui/FlashActivity.java
+++ app/src/main/java/vn/congphuong/ominext/soanvanthpt/ui/FlashActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import vn.congphuong.ominext.soanvanthpt.R;
 
@@ -11,6 +13,9 @@ public class FlashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_flash);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -20,7 +25,7 @@ public class FlashActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 1000);
+        }, 1500);
     }
 
 
